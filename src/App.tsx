8e84ad83d3b01ed0ef428245f452a53f367cfa0e
Oleg Ivanov <gmiolegv@gmail.com>
2022-02-11 16:46:43 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React, {useEffect, useRef} from 'react';
+import clsx from 'clsx';
+
+import './App.css';
+
+function App() {
+
+    const resInput = useRef(null)
+
+    const [value, setValue] = React.useState(0);
+
+
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <>
+        </>
+    );
+}
+
+export default App;
